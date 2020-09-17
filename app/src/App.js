199c3898f7cddfa05b8565f8341799cad3a9bcd3
Philip <philip@real.domain.smileyface>
2020-09-17 10:44:01 +0200

    created header
    created header, added react routing, and material ui

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import About from './About';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+        </Switch>
+      </Router>
       <p>A water footprint shows the extent of water use in relation to consumption by people. The water footprint of an individual, community or business is defined as the total volume of fresh water used to produce the goods and services consumed by the individual or community or produced by the business. Water use is measured in water volume consumed (evaporated) and/or polluted per unit of time. A water footprint can be calculated for any well-defined group of consumers (e.g., an individual, family, village, city, province, state or nation) or producers (e.g., a public organization, private enterprise or economic sector), for a single process (such as growing rice) or for any product or service.</p>
     </div>
   );
