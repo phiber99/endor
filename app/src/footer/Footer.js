bc709e6b61b2554c2588173dbfadfd63cc321695
Philip <philip@real.domain.smileyface>
2020-09-27 19:31:56 +0200

    suppressed warnings from footer

--- app/src/footer/Footer.js
+++ app/src/footer/Footer.js
@@ -1,37 +1,46 @@
 import React from "react"
 import './footer.css'
 
-function Footer(){
+function Footer() {
     return (
         <footer>
-        <div className="footer-wrapper">
-            <div className="single-footer">
-                <h2>Navigate</h2>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ul>
-            </div>
-            <div className="single-footer">
-                <h2>Useful links</h2>
-                <ul>
-                    <li><a href="#">Something 1</a></li>
-                    <li><a href="#">Something 2</a></li>
-                    <li><a href="#">Something 3</a></li>
-                </ul>
-            </div>
-            <div className="single-footer">
-                <h2>Useful links 2</h2>
-                <ul>
-                    <li><a href="#">Something 1</a></li>
-                    <li><a href="#">Something 2</a></li>
-                    <li><a href="#">Something 3</a></li>
-                </ul>
-            </div>
+            <div className="footer-wrapper">
+                <div className="single-footer">
+                    <h2>Navigate</h2>
+                    <ul>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Home</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">About</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Facebook</a></li>
+                    </ul>
+                </div>
+                <div className="single-footer">
+                    <h2>Useful links</h2>
+                    <ul>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Something 1</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Something 2</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Something 3</a></li>
+                    </ul>
+                </div>
+                <div className="single-footer">
+                    <h2>Useful links 2</h2>
+                    <ul>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Something 1</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Something 2</a></li>
+                        {/* eslint-disable-next-line */}
+                        <li><a href="#">Something 3</a></li>
+                    </ul>
+                </div>
 
-        </div>
-    </footer>
+            </div>
+        </footer>
     )
 }
 
