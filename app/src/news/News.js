42d3abacb1d75d9cd9e4342f0b82dcc8de789fe7
ALX99 <46844683+ALX99@users.noreply.github.com>
2020-10-03 12:20:45 +0200

    Create loadingscreen
    Co-authored-by: tvigg@hotmail.com <tvigg@hotmail.com>

--- app/src/news/News.js
+++ app/src/news/News.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import NewsCard from "./NewsCard";
 import { makeStyles } from "@material-ui/core/styles";
+import React, { useEffect, useState } from "react";
+import LoadingScreen from "../loadingscreen/Loadingscreen";
 import { fetchNews } from "./FetchReddit";
+import NewsCard from "./NewsCard";
 
 const useStyles = makeStyles({
   root: {
@@ -14,28 +15,34 @@ const useStyles = makeStyles({
 
 export default function News() {
   const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
   const classes = useStyles();
 
   // Only fetch the news on component mount
   useEffect(() => {
     const newsEffect = async () => {
       const newsItems = await fetchNews();
+      // setTimeout(() => { //to make the loading screen appear for 1,5 sec
       setNews(newsItems);
+      setLoading(false)
+      // }, 1500)           // I belong to the comment over me.
     };
-
     newsEffect();
   }, []);
 
-  return (
-    <div className={classes.root} data-testid="news">
-      {news.map((newsItem, i) => (
-        <NewsCard
-          key={i}
-          summary={newsItem.title}
-          thumbnail={newsItem.thumbnail}
-          url={newsItem.url}
-        />
-      ))}
-    </div>
-  );
+  if (!loading)
+    return (
+      <div className={classes.root} data-testid="news">
+        {news.map((newsItem, i) => (
+          <NewsCard
+            key={i}
+            summary={newsItem.title}
+            thumbnail={newsItem.thumbnail}
+            url={newsItem.url}
+          />
+        ))}
+      </div>
+    );
+  else
+    return <LoadingScreen />
 }
