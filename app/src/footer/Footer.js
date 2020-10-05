e63219ea7dc1a9cda410c09bd837443325340ed0
Philip <philip@real.domain.smileyface>
2020-10-05 23:02:12 +0200

    wrapped in container

--- app/src/footer/Footer.js
+++ app/src/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react"
 import './footer.css'
-import { Grid, Typography } from '@material-ui/core'
+import { Container, Grid, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -18,44 +18,44 @@ const Footer = () => {
 
   return (
     <footer className={classes.footer}>
-      <Grid
-        container
-        spacing={2}
-        justify="center">
-        <Grid item xs={10} md={3}>
-          <Typography variant="h5" className={classes.text} >Navigate</Typography>
-          <ul className="footer-ul">
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Home</a></li>
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>About</a></li>
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Facebook</a></li>
-          </ul>
+      <Container
+        maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h5" className={classes.text} >Navigate</Typography>
+            <ul className="footer-ul">
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Home</a></li>
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>About</a></li>
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Facebook</a></li>
+            </ul>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h5" className={classes.text}>Useful links</Typography>
+            <ul className="footer-ul">
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Something 1</a></li>
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Something 2</a></li>
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Something 3</a></li>
+            </ul>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h5" className={classes.text}>Useful links 2</Typography>
+            <ul className="footer-ul">
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Something 1</a></li>
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Something 2</a></li>
+              {/* eslint-disable-next-line */}
+              <li><a href="#" className={classes.text}>Something 3</a></li>
+            </ul>
+          </Grid>
         </Grid>
-        <Grid item xs={10} md={3}>
-          <Typography variant="h5" className={classes.text}>Useful links</Typography>
-          <ul className="footer-ul">
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Something 1</a></li>
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Something 2</a></li>
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Something 3</a></li>
-          </ul>
-        </Grid>
-        <Grid item xs={10} md={3}>
-          <Typography variant="h5" className={classes.text}>Useful links 2</Typography>
-          <ul className="footer-ul">
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Something 1</a></li>
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Something 2</a></li>
-            {/* eslint-disable-next-line */}
-            <li><a href="#" className={classes.text}>Something 3</a></li>
-          </ul>
-        </Grid>
-      </Grid>
+      </Container>
     </footer>
   )
 }
